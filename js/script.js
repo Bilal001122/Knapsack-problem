768124764993e871bc0fdc90c416fb420c8c84b9
Bilal001122 <kb_arab@esi.dz>
2023-11-23 16:35:37 +0100

    90%

--- js/script.js
+++ js/script.js
@@ -6,6 +6,46 @@ const tableBody = document.querySelector("table tbody");
 const enteredCapacity = document.querySelector("#poids-max");
 const resultText = document.querySelector("#result");
 const bigContainer = document.querySelector(".container");
+const closeImageButton = document.querySelector(".close-img");
+const overlay = document.querySelector(".overlay");
+const resultContainer = document.querySelector(".result-container");
+
+const closeImage = function () {
+  bigContainer.classList.remove("add-blur");
+  resultText.parentElement.parentElement.classList.add("hidden");
+  overlay.style.display = "none";
+  document.querySelector(".result-table-container").lastChild.remove();
+};
+
+const knapsackAlgorithm = function (
+  arrayOfProfits,
+  arrayOfWeights,
+  maximumCapacity,
+  numberOfObjects
+) {
+  let globalArray = []; // Initialise globalArray
+  arrayOfProfits.unshift(0); // Ajoute un 0 au début du tableau des profits
+  arrayOfWeights.unshift(0); // Ajoute un 0 au début du tableau des poids
+  for (let i = 0; i <= numberOfObjects; i++) {
+    globalArray[i] = []; // Initialise les sous-tableaux
+    for (let w = 0; w <= maximumCapacity; w++) {
+      if (i === 0 || w === 0) {
+        globalArray[i][w] = 0;
+      } else if (arrayOfWeights[i] <= w) {
+        console.log(arrayOfWeights[i], w);
+        globalArray[i][w] = Math.max(
+          arrayOfProfits[i] + globalArray[i - 1][w - arrayOfWeights[i]],
+          globalArray[i - 1][w]
+        );
+      } else {
+        globalArray[i][w] = globalArray[i - 1][w];
+      }
+    }
+  }
+  console.log(arrayOfProfits);
+  console.log(arrayOfWeights);
+  return globalArray;
+};
 
 const addNewObject = function () {
   const currentObjectNumber = tableBody.children.length + 1;
@@ -30,19 +70,24 @@ const removeObject = function (e) {
   }
 };
 
-addObjectbtn.addEventListener("click", addNewObject);
-
 tableBody.addEventListener("click", function (e) {
   removeObject(e);
 });
 
+closeImageButton.addEventListener("click", closeImage);
+
+addObjectbtn.addEventListener("click", addNewObject);
+
 calculateResultBtn.addEventListener("click", function (e) {
-  e.preventDefault();
   bigContainer.classList.add("add-blur");
+  overlay.style.display = "block";
   resultText.parentElement.parentElement.classList.remove("hidden");
+
   const objects = [];
-  const capacity = enteredCapacity.value;
-  const objectRowsTable = [...document.querySelectorAll("table tbody tr")];
+  const maximumCapacity = Number(enteredCapacity.value);
+  const objectRowsTable = [
+    ...document.querySelectorAll(".table-principale tbody tr"),
+  ];
   objectRowsTable.forEach((objectRow) => {
     const objectName = objectRow.querySelector("input[type=text]").value;
     const objectWeight = Number(
@@ -53,30 +98,34 @@ calculateResultBtn.addEventListener("click", function (e) {
     );
     objects.push({ objectName, objectWeight, objectProfit });
   });
-  console.log(objects);
 
-  // objects.sort(
-  //   (a, b) => b.objectProfit / b.objectWeight - a.objectProfit / a.objectWeight
-  // );
+  const arrayOfProfits = objects.map((object) => object.objectProfit);
+  const arrayOfWeights = objects.map((object) => object.objectWeight);
+  const numberOfObjects = arrayOfProfits.length;
+  console.log(arrayOfProfits, arrayOfWeights, maximumCapacity, numberOfObjects);
+  const globalArray = knapsackAlgorithm(
+    arrayOfWeights,
+    arrayOfProfits,
+    maximumCapacity,
+    numberOfObjects
+  );
+  const tableContainer = document.createElement("div");
+  const resultTable = document.createElement("table");
+  tableContainer.classList.add("table-container");
+  resultTable.classList.add("table-de-calcul");
 
-  // const n = objects.length;
-  // const dp = Array(n + 1)
-  //   .fill()
-  //   .map(() => Array(capacity + 1).fill(0));
-
-  // for (let i = 1; i <= n; i++) {
-  //   for (let w = 1; w <= capacity; w++) {
-  //     if (objects[i - 1].objectWeight <= w) {
-  //       dp[i][w] = Math.max(
-  //         dp[i - 1][w],
-  //         dp[i - 1][w - objects[i - 1].objectWeight] +
-  //           objects[i - 1].objectProfit
-  //       );
-  //     } else {
-  //       dp[i][w] = dp[i - 1][w];
-  //     }
-  //   }
-  // }
-  // console.log(dp[n][capacity]);
-  // resultText.textContent = dp[n][capacity];
+  tableContainer.appendChild(resultTable);
+  resultTable.appendChild(document.createElement("tbody"));
+  globalArray.forEach((row) => {
+    const newRow = document.createElement("tr");
+    row.forEach((element) => {
+      const td = document.createElement("td");
+      td.textContent = element;
+      td.style.color = "black";
+      newRow.appendChild(td);
+    });
+    resultTable.firstChild.appendChild(newRow);
+  });
+  document.querySelector(".result-table-container").appendChild(tableContainer);
+  resultText.textContent = globalArray[numberOfObjects][maximumCapacity];
 });
